c3bc0571a37f036f0f82a6dc32d05e36b190da20
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-09-07 20:57:44 -0300

    update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import Data from "../components/Data/Data";
+import Header from "../components/Header/Header";
+import { Context } from "../components/Context/Context";
+
+const Home = (props) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1024);
+    };
+
+    // Adiciona um ouvinte de evento para redimensionamento da janela
+    window.addEventListener("resize", handleResize);
+
+    // Remove o ouvinte de evento quando o componente é desmontado
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <Context.Provider value={{ isMobile, setIsMobile }}>
+      <section>
+        {isMobile ? (
+          <Header
+            data={
+              props.data ||
+              Data.find((e) => e.headerMobile).headerMobile.find(
+                (e) => e.informations
+              ).informations
+            }
+          />
+        ) : (
+          <Header />
+        )}
+      </section>
+    </Context.Provider>
+  );
+};
+
+export default Home;
---
+++ src/components/Data/Data.js
@@ -0,0 +1,30 @@
+const Data = [
+  {
+    headerMobile: [
+      {
+        informations: [
+          {
+            icon: "../imgs/iconMenuMobile.svg",
+            name: "MENU",
+          },
+          {
+            icon: "../imgs/logoMain.svg",
+          },
+          {
+            icon: "../imgs/iconMenuMobileClose.svg",
+            name: "FECHAR",
+          },
+          {
+            icon: "../imgs/iconSearch.svg",
+          },
+          {
+            icon: "../imgs/iconBag.svg",
+            number: "12",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default Data;
